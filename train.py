b996aba747670588c396906a3a457ea3d20eaa6d
Keaton Nordby <keatonnordby4@gmail.com>
2025-07-17 21:54:03 -0500

    Initialize Modal app with pip/apt installs, ESC-50 dataset fetch, and parallel function execution

--- train.py
+++ train.py
@@ -2,7 +2,14 @@ import sys
 
 import modal
 
-app = modal.App("example-hello-world")
+app = modal.App("audio-cnn")
+
+image = (modal.Image.debian_slim()
+        .pip_install_from_requirements("requirements.txt")
+        .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
+        .run_commands([
+            "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip"
+        ]))
 
 @app.function()
 def f(i):
@@ -16,10 +23,10 @@ def f(i):
 @app.local_entrypoint()
 def main():
     # run the function locally
-    print(f.local(1000))
+    print(f.local(10))
 
     # run the function remotely on Modal
-    print(f.remote(1000))
+    print(f.remote(10))
 
     # run the function in parallel and remotely on Modal
     total = 0
