9bc8bfbd9bec0715c21f97164c817dce323c77e1
Keaton Nordby <keatonnordby4@gmail.com>
2025-07-20 18:46:02 -0500

    training model in work

--- train.py
+++ train.py
@@ -1,6 +1,14 @@
 import sys
 
+import pandas as pd
+import torchaudio
+import torch
+
 import modal
+from torch.utils.data import Dataset
+from pathlib import Path
+import torch.nn as nn
+import torchaudio.transforms as T
 
 app = modal.App("audio-cnn")
 
@@ -19,11 +27,81 @@ image = (modal.Image.debian_slim()
 volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
 model_volume = modal.Volume.from_name("esc-model", create_if_missing=True)
 
+class ESC50Dataset(Dataset):
+    def __init__(self, data_dir, metadata_file, split='train', transform=None):
+        super().__init__()
+        self.data_dir = Path(data_dir)
+        self.metadata = pd.read_csv(metadata_file)
+        self.split = split
+        self.transform = transform
+
+        if split == 'train':
+            self.metadata = self.metadata[self.metadata['fold'] != 5]
+        else:
+            self.metadata = self.metadata[self.metadata['fold'] == 5]
+
+        self.classes = sorted(self.metadata['category'].unique())
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+        self.metadata['label'] = self.metadata['category'].map(
+            self.class_to_idx)
+        
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+        audio_path = self.data_dir / "audio" / row['filename']
+
+        waveform, sample_rate = torchaudio.load(audio_path)
+        # [channels, smaples] = [2, 44000] -> reduce it down to a 0 and find the mean of the two channels
+
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+        
+        if self.transform:
+            spectrogram = self.transform(waveform)
+        else:
+            spectrogram = waveform
+
+        return spectrogram, row['label']
+
 
 @app.function(image=image, gpu="A10G", volumes={"/data": volume, "/models": model_volume}, timeout=60 * 60 * 3)
 def train():
-    print("Training")
+    esc50_dir = Path("/opt/esc50-data")
+    train_transform = nn.Sequential(
+        T.MelSpectrogram(
+            sample_rate=22050,
+            n_fft=1024,
+            hop_length=512,
+            n_mels=128,
+            f_min=0,
+            f_max=11025
+        ),
+        T.AmplitudeToDB(),
+        T.FrequencyMasking(freq_mask_param=30),
+        T.TimeMasking(time_mask_param=80)  
+    )
+
+    val_transform = nn.Sequential(
+        T.MelSpectrogram(
+            sample_rate=22050,
+            n_fft=1024,
+            hop_length=512,
+            n_mels=128,
+            f_min=0,
+            f_max=11025
+        ),
+        T.AmplitudeToDB()
+    )
+
+    train_dataset = ESC50Dataset(
+        data_dir=esc50_dir, metadata_file=esc50_dir / "meta" / "esc50.csv", split="train", transform=train_transform)
+    val_dataset = ESC50Dataset(
+        data_dir=esc50_dir, metadata_file=esc50_dir / "meta" / "esc50.csv", split="val", transform=val_transform)
     
+    print("Training samples: " + len(train_dataset))
+    print("Validation samples: " + len(val_dataset))
 
 @app.local_entrypoint()
 def main():
